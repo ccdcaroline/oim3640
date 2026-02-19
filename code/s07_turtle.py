1b12f79ae7db527d0a477dd5f17ea39e9b9d6075
ccdcaroline <cdigiovanni1@babson.edu>
2026-02-19 09:58:28 -0500

    turtle code

---
+++ code/s07_turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+def draw_square(turtle_obj, size = 100):
+    """Draw a square with the given size."""
+    for i in range(4):
+        turtle_obj.forward(size)
+        turtle_obj.left(90)
+
+def draw_spiral(t): 
+    """
+    Draw one square, turn a angle, then draw another suare and so on""" 
+    for i in range(72): 
+        draw_square(t, 50 + i * 2)
+        t.left(5)
+turtle_done()
+
